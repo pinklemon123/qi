378a63f18bd3921f04a0c0fa3a2c27d647995042
pinklemon123 <15893966861@163.com>
2025-09-06 10:55:46 +0800

    lty4.0.9

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,11 +1,11 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "你的 Supabase URL";
-const SUPABASE_ANON_KEY = "你的 anon key";
+const SUPABASE_URL = "https://wtscvbbibduvccpudytb.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0c2N2YmJpYmR1dmNjcHVkeXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMTk5MjMsImV4cCI6MjA3MjY5NTkyM30.Y12kKHZN8WZPr8u00D7U_n0F3Ix88Rofihj3QAfw_ak";
 
 window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    flowType: 'implicit',         // 关键：改为 implicit，支持跨设备魔法链接
+    flowType: 'implicit',
     detectSessionInUrl: true,
     persistSession: true,
     autoRefreshToken: true,
